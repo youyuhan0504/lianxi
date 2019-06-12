60fa12c2706a55482a2c11671f605c368b8870ff
youyuhan <1049323237@qq.com>
2019-06-12 16:47:59 +0800

    项目项目123

---
+++ src/main/java/com/bw/controller/Mycontroller.java
@@ -0,0 +1,16 @@
+package com.bw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Mycontroller {
+
+	@RequestMapping("list")
+	@ResponseBody
+	public Object list() {
+		return true;
+		
+	}
+}
